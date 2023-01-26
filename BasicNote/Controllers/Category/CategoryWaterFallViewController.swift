1a7cfe9d0db920b829a43caf6bc9d0c39b61c26e
Anıl Avcı <anilavci52@gmail.com>
2023-01-26 17:19:21 +0300

    Create CategoryviewController

---
+++ BasicNote/Controllers/Category/CategoryWaterFallViewController.swift
@@ -0,0 +1,36 @@
+//
+//  CategoryViewController.swift
+//  BasicNote
+//
+//  Created by Anıl AVCI on 26.01.2023.
+//
+
+import UIKit
+
+class CategoryWaterFallViewController: UIViewController {
+    @IBOutlet weak var collectionWall: UICollectionView!
+    let cell = "wallCell"
+    override func viewDidLoad() {
+        super.viewDidLoad()
+//        collectionWall.delegate = self
+//        collectionWall.dataSource = self
+        
+        let nibCell = UINib(nibName: "WaterFallCollectionViewCell", bundle: nil)
+        collectionWall.register(nibCell, forCellWithReuseIdentifier: cell)
+    }
+    
+
+
+}
+//extension CategoryWaterFallViewController: UICollectionViewDelegate,UICollectionViewDataSource {
+//    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+//        return 10
+//    }
+//
+//    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+//        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cell, for: indexPath) as? WaterFallCollectionViewCell
+//        cell?.textLabel.text = "Naber"
+//        return cell!
+//    }
+//}
+
---
+++ BasicNote/Controllers/Category/WaterFallCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  WaterFallCollectionViewCell.swift
+//  BasicNote
+//
+//  Created by Anıl AVCI on 26.01.2023.
+//
+
+import UIKit
+
+class WaterFallCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var textLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
