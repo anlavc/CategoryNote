3b3ed561934dd2e18435d9fb238b51a07f38db2e
Anıl Avcı <anilavci52@gmail.com>
2023-02-02 23:30:31 +0300

    hex color try

--- BasicNote/Controllers/Category/CategoryWaterFallViewController.swift
+++ BasicNote/Controllers/Category/CategoryWaterFallViewController.swift
@@ -12,8 +12,8 @@ class CategoryWaterFallViewController: UIViewController {
     let cell = "wallCell"
     override func viewDidLoad() {
         super.viewDidLoad()
-//        collectionWall.delegate = self
-//        collectionWall.dataSource = self
+        collectionWall.delegate = self
+        collectionWall.dataSource = self
         
         let nibCell = UINib(nibName: "WaterFallCollectionViewCell", bundle: nil)
         collectionWall.register(nibCell, forCellWithReuseIdentifier: cell)
@@ -22,15 +22,15 @@ class CategoryWaterFallViewController: UIViewController {
 
 
 }
-//extension CategoryWaterFallViewController: UICollectionViewDelegate,UICollectionViewDataSource {
-//    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-//        return 10
-//    }
-//
-//    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-//        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cell, for: indexPath) as? WaterFallCollectionViewCell
-//        cell?.textLabel.text = "Naber"
-//        return cell!
-//    }
-//}
+extension CategoryWaterFallViewController: UICollectionViewDelegate,UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cell, for: indexPath) as? WaterFallCollectionViewCell
+        cell?.textLabel.text = "Naber"
+        return cell!
+    }
+}
 
